7f1a3fdd92d9b60f8ac732554a6b08be865956ab
Egor1-0 <webpanda1301@gmail.com>
2024-08-05 21:24:34 +0300

    create data file

--- app/handlers.py
+++ app/handlers.py
@@ -4,9 +4,11 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup 
+
 from app.database.push import set_word, update_guess_word, auto_decriment_lifes, insert_letter
 from app.database.requests import get_row, get_guess_word, is_win, is_lose, get_lifes, get_used_letters
 from app.database.delete import delete_session
+from app.data import gallows
 
 file = open('words/words.txt')
 words_list = []
@@ -14,13 +16,7 @@ for line in file:
     words_list.extend(line.split())
 file.close()
 
-gallows = ['\n    &&&&&&&&&&&&&&&& \n    &              | \n    &              | \n    &              | \n    &              O \n    &            / | \\ \n    &             / \\ \n    & \n    & \n    & \n&&&&&&&&&', 
-           '\n    &&&&&&&&&&&&&&&& \n    &              | \n    &              | \n    &              | \n    &              O \n    &            / | \\ \n    &             \n    & \n    & \n    & \n&&&&&&&&&', 
-           '\n    &&&&&&&&&&&&&&&& \n    &              | \n    &              | \n    &              | \n    &              O \n    &              | \n    &             \n    & \n    & \n    & \n&&&&&&&&&', 
-           '\n    &&&&&&&&&&&&&&&& \n    &              | \n    &              | \n    &              | \n    &               \n    &              \n    &             \n    & \n    & \n    & \n&&&&&&&&&', 
-           '\n    &&&&&&&&&&&&&&&& \n    &              \n    &              \n    &              \n    &               \n    &              \n    &             \n    & \n    & \n    & \n&&&&&&&&&', 
-           '\n& \n& \n& \n& \n& \n& \n& \n& \n& \n& \n&&&&&&&&&', 
-           '\n& \n& \n& \n& \n& \n& \n& \n& \n& \n& \n&']
+
 
 router = Router()
 
